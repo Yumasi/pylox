8252646ca9506b641b09d35d420b0fd39d7707f7
Guillaume Pagnoux <gpagnoux@gmail.com>
2023-04-01 12:53:37 +0200

    add flake
    Signed-off-by: Guillaume Pagnoux <gpagnoux@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A flake for a Python project with Poetry";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = with pkgs; mkShell { buildInputs = [ python3 poetry ]; };
+      });
+}
